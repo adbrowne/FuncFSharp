994d05ab9d404c9ba0574d397f6cd67d982634be
Andrew Browne <andrew@importwizard.net>
2013-04-28 22:07:14 +1000

    Getting started implemented

--- FuncFSharp/GettingStarted.fs
+++ FuncFSharp/GettingStarted.fs
@@ -9,7 +9,6 @@
                 match gt(x1,x2) with
                 | true -> false
                 | false -> isSorted (x2::xs) gt
-                
     [<Test>]
     let ``Empty returns true`` () : unit =
         Assert.IsTrue(isSorted [] (fun (a,b) -> a > b))
@@ -25,3 +24,25 @@
     [<Test>]
     let ``Orderd list returns true`` () : unit =
         Assert.IsTrue(isSorted [1;2] (fun (a,b) -> a > b))
+
+    let partial1 a f : 'b -> 'c =
+        fun b -> f(a,b)     
+
+    [<Test>]
+    let ``Partial application`` () : unit =
+        let firstPart = partial1 1 (fun (x,y) -> x + y) 
+        let result = firstPart 2
+        Assert.AreEqual(3, result)
+
+    let curry (f:('a*'b) -> 'c) : 'a -> ('b -> 'c) =
+        fun a ->
+            fun b ->
+                f(a, b)
+
+    let uncurry (f:'a -> 'b -> 'c) : ('a*'b) -> 'c =
+        fun (a, b) ->
+            f(a)(b)
+
+    let compose (f: 'b -> 'c) (g:'a -> 'b) : 'a -> 'c =
+        fun x ->
+            f(g(x))
