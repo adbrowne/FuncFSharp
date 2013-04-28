7aba95e9ae20afd383f35b53846e470ee118e394
Andrew Browne <andrew@importwizard.net>
2013-04-28 21:42:30 +1000

    Implemented Exercise 2

---
+++ FuncFSharp/GettingStarted.fs
@@ -0,0 +1,27 @@
+﻿module GettingStarted
+    open NUnit.Framework
+
+    let rec isSorted (list: list<'a>) (gt: ('a*'a) -> bool) : bool =   
+        match list with
+            | [] -> true
+            | x::[] -> true
+            | x1::x2::xs -> 
+                match gt(x1,x2) with
+                | true -> false
+                | false -> isSorted (x2::xs) gt
+                
+    [<Test>]
+    let ``Empty returns true`` () : unit =
+        Assert.IsTrue(isSorted [] (fun (a,b) -> a > b))
+
+    [<Test>]
+    let ``Single item returns true`` () : unit =
+        Assert.IsTrue(isSorted [1] (fun (a,b) -> a > b))
+
+    [<Test>]
+    let ``Unorderd list returns false`` () : unit =
+        Assert.IsFalse(isSorted [2;1] (fun (a,b) -> a > b))
+
+    [<Test>]
+    let ``Orderd list returns true`` () : unit =
+        Assert.IsTrue(isSorted [1;2] (fun (a,b) -> a > b))
