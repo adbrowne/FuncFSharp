0418ec1ca758951415c0c7b74bd43dbca23f0dfd
Andrew Browne <andrew@importwizard.net>
2013-04-28 20:46:06 +1000

    Started monoid work

---
+++ FuncFSharp/Monoid.fs
@@ -0,0 +1,40 @@
+﻿namespace FuncFSharp
+
+type IMonoid<'T> =
+  abstract member mempty  : unit -> 'T
+  abstract member mappend : 'T * 'T -> 'T
+
+type intAdditionMonoid =
+    interface IMonoid<int> with
+        member this.mempty() = 0
+        member this.mappend(a, b) = a + b
+
+type intMultiplicationMonoid =
+    interface IMonoid<int> with
+        member this.mempty() = 1
+        member this.mappend(a, b) = a * b
+
+type booleanOr =
+    interface IMonoid<bool> with
+        member this.mempty() = false
+        member this.mappend(a, b) = a || b
+
+type booleanAnd =
+    interface IMonoid<bool> with
+        member this.mempty() = true
+        member this.mappend(a, b) = a && b
+
+type optionMonoid<'a> =
+    interface IMonoid<option<'a>> with
+        member this.mempty() = None
+        member this.mappend(a, b) =
+            match a with 
+            | None -> b
+            | _ -> a
+
+module MonoidTests =
+    open NUnit.Framework
+    [<Test>] 
+    let ``Some Test`` () : unit = 
+            let b = Some "Andrew" : option<string>
+            Assert.False(true)
